bcca7d68397ac681b71a116fe939ab14211c9cac
my name <me@example.com>
2023-04-06 13:36:28 +0700

    Add a simple network connectivity

--- disk_check.py
+++ disk_check.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 import sys
+import socket
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
@@ -16,13 +19,22 @@ def check_disk_full(disk, min_gb, min_percent):
     return False
 
 def check_root_full():
-    """Returns True if the root partition is full, FAlse otherwise."""
+    """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full")
+        (check_root_full, "Root partition full"),
+        (check_no_network, "No working network.")
     ]
     everything_ok = True
     for check, msg in checks:
@@ -31,7 +43,7 @@ def main():
             sys.exit(1)
             everything_ok = False
 
-            
+
     if not everything_ok:
         sys.exit(1)
 
