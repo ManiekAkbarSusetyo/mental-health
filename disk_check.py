88e239b917437f7e236a2203aa079cc1ad972b22
my name <me@example.com>
2023-04-06 12:17:23 +0700

    Allow printing more than one error msg

--- disk_check.py
+++ disk_check.py
@@ -18,15 +18,22 @@ def check_disk_full(disk, min_gb, min_percent):
 def check_root_full():
     """Returns True if the root partition is full, FAlse otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main():
     checks=[
-    (check_reboot, "Pending Reboot"),
-    (check_root_full, "Root partition full")
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full")
     ]
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
             sys.exit(1)
+            everything_ok = False
+
+            
+    if not everything_ok:
+        sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
